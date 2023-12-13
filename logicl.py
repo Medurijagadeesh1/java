46caa5b1075fc4e629e3c9ea7f4e4a9f8d278267
Meduri Jagadeesh <113991109+Medurijagadeesh1@users.noreply.github.com>
2023-12-13 14:25:20 +0530

    logical operators

---
+++ logicl.py
@@ -0,0 +1,29 @@
+a = 10
+b = 20
+c = 30
+
+if a > b and a > c: 
+	print(" a is large") 
+
+elif b>a and b>c: 
+	print(" b is large") 
+else:
+    print("c is large")
+    
+#logical or
+
+a=10
+b=30
+c=20
+if b>a or a>c:
+    print("one is correct")
+else:
+    print("no one is correct")
+
+#not
+my_list = [1, 2, 3, 4, 5]
+
+
+if not 6 in my_list:
+    print("6 is not my_list")
+

ac4bbca14fd578fcbf5fc7e56dbb02d230922b89
Meduri Jagadeesh <113991109+Medurijagadeesh1@users.noreply.github.com>
2023-12-14 14:11:22 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,36 @@
+def jagadeesh():
+    print("jagadeesh")
+    
+jagadeesh()
+# ##############
+def art(a,b):
+    print(a+b)
+    print(a*b)
+    print(a-b)
+    print(a/b)
+    print(a%b)
+art(8,9)
+#######################
+def jag(*a):
+    print(a)
+jag(1,2,3,4,5)
+#######################
+num=-33
+print(abs(num))
+list=[1,2,3]
+print(len(list))
+print(max(list))
+print(len(list))
+##########################
+def add(x,y):
+    return x+y
+result=add(3,5)
+print(result)
+###################
+def num(a):
+        if(a%2==0):
+            print("even")
+        else:
+            print("odd")
+num(int(input("enter a number")))
+################################
